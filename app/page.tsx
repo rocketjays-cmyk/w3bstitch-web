074135e91c112735b4fdd8d9f501848e8612067e
rocketjays-cmyk <rocketjays@gmail.com>
2025-09-06 12:53:34 -0400

    fix: consolidate to src/app and restore original UI

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/page.tsx
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>W3b Stitch</h1>
-      <p>Home is alive. See <a href="/debug/routes">/debug/routes</a> to list routes.</p>
-    </main>
-  );
-}
