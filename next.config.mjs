eb3e9c0d5a8e79d84a132ff54d0408250b3a8063
rocketjays-cmyk <rocketjays@gmail.com>
2025-09-07 17:50:27 -0400

    chore: align eslint v8 with eslint-config-next 14.2.32

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // turn this on only if you need to bypass lint during builds
+  // eslint: { ignoreDuringBuilds: true },
+
+  // if you added any runtime tweaks, keep them here
+  // experimental: {},
+};
+
+export default nextConfig;
