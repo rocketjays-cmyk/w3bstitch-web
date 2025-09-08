c5be0eae9055d85fd591a514e94abbc94f157ad1
Jayden C. Saliby <rocketjays@gmail.com>
2025-09-08 12:34:19 -0400

    feat: anchor via wallet extension

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,1 +1,15 @@
+"use client";
 
+import { useRouter } from "next/navigation";
+
+export default function BackButton() {
+  const router = useRouter();
+  return (
+    <button
+      onClick={() => router.back()}
+      className="absolute top-4 left-4 px-3 py-1 border rounded"
+    >
+      ← Back
+    </button>
+  );
+}
